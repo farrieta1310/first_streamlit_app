399e4a95526990c2e13410a748a3b4279af0fa72
farrieta1310 <135244536+farrieta1310@users.noreply.github.com>
2023-06-01 09:07:20 -0600

    Update streamlit_app.py
    added markdown lines

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,3 +2,6 @@ import streamlit
 
 streamlit.title('My Parents New Healthy Diner')
 streamlit.header('Breakfast Menu')
+streamlit.markdown('Omega 3 & Blueberry Oatmeal')
+streamlit.markdown('Kale, Spinach & Rocket Smoothie')
+streamlit.markdown('Hard-Boiled Free-Fange Egg')
