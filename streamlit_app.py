fac99eb7fc8dffd46099b3247792288e0c7267cd
farrieta1310 <135244536+farrieta1310@users.noreply.github.com>
2023-06-01 09:10:34 -0600

    Update streamlit_app.py
    replace markdown for text
    updated breakfast items
    added a new breakfast item
    added a new header

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,7 +1,9 @@
 import streamlit
 
 streamlit.title('My Parents New Healthy Diner')
-streamlit.header('Breakfast Menu')
-streamlit.markdown('Omega 3 & Blueberry Oatmeal')
-streamlit.markdown('Kale, Spinach & Rocket Smoothie')
-streamlit.markdown('Hard-Boiled Free-Fange Egg')
+streamlit.header('Breakfast Favorites')
+streamlit.text('🥣 Omega 3 & Blueberry Oatmeal')
+streamlit.text('🥗 Kale, Spinach & Rocket Smoothie')
+streamlit.text('🐔 Hard-Boiled Free-Fange Egg')
+streamlit.text('🥑🍞 Avocado Toast')
+streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
