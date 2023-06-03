6149d8a66c10b71839605b0750214818a1e957cb
farrieta1310 <135244536+farrieta1310@users.noreply.github.com>
2023-06-03 07:52:22 -0600

    Update streamlit_app.py
    Changed the cursor to return all rows from snowflake query, instead of just the first result

--- streamlit_app.py
+++ streamlit_app.py
@@ -36,8 +36,8 @@ streamlit.dataframe(fruityvice_normalized)
 import snowflake.connector
 
 my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
-my_cur = my_cnx.cursor()
+my_cur = my_cnx.fetchall()
 my_cur.execute("SELECT * FROM FRUIT_LOAD_LIST")
 my_data_row = my_cur.fetchone()
 streamlit.header("The fruit load list contains:")
-streamlit.dataframe(my_data_row)
+streamlit.dataframe(my_data_rows)
